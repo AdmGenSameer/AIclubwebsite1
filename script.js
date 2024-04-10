e459ace98f589b321701fc869c0003123c9fb849
AdmGenSameer <154604600+AdmGenSameer@users.noreply.github.com>
2024-04-10 22:16:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle mobile navigation menu
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('show');
+  });
+  
